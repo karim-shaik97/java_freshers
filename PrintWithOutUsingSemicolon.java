7d751ebbb255e0b05215fb8de5f12d43347dbe77
Karimulla Sha <55952608+karim-shaik97@users.noreply.github.com>
2019-10-09 22:28:45 +0800

    Add files via upload

---
+++ PrintWithOutUsingSemicolon.java
@@ -0,0 +1,16 @@
+public class PrintWithOutUsingSemicolon {
+    private boolean hello;
+    public static void main(String args[]) {
+       /* if(System.out.printf("Hello World") == null) {
+        }*/
+
+    }
+
+    public void setBooleanValue(){
+        hello = true;
+    }
+
+    public boolean getBooleanValue() {
+        return hello;
+    }
+}
